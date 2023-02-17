401f23ef672247b5cb572be58166c98e4d53313c
KievMaia <kievmaia@gmail.com>
2023-02-17 18:45:40 -0300

    4.11. Mapeando relacionamentos um-para-um com @JoinTable

--- src/main/java/com/algaworks/ecommerce/model/NotaFiscal.java
+++ src/main/java/com/algaworks/ecommerce/model/NotaFiscal.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
@@ -31,6 +32,10 @@ public class NotaFiscal {
 	private Integer id;
 	
 	@OneToOne
+	@JoinColumn(name = "pedido_id")
+//	@JoinTable(name = "pedido_nota_fiscal", 
+//				joinColumns = @JoinColumn(name = "nota_fiscal_id", unique = true), 
+//				inverseJoinColumns = @JoinColumn(name = "pedido_id", unique = true))
 	private Pedido pedido;
 	
 	private String xml;
